41bed24c5c565672e0c05a38d0328a73e4e9397e
LOPEZ Gabriel <gabriel.lopez@epitech.eu>
2024-06-19 21:01:43 +0200

    config( backend ): Add husky to manage git hooks

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NEST_APP_PORT ?? 3000);
+  await app.listen(3000);
 }
-
 bootstrap();
