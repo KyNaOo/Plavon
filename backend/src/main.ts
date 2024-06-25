67eecd2aa8e3788bd0292c6096261d6e24fc4a0b
Gabriel Lopez <91618272+glopez-dev@users.noreply.github.com>
2024-06-25 14:36:25 +0200

    Setup the backend (#3)
    * docs( README.md ): Update the backend installation directives.
    
    * config( backend ): Install the dotenv module.
    
    * refactor( backend ): Add a docstring and an environment variable for the main.ts module.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+/**
+ * The entry file of the application which uses the core function
+ * NestFactory to create a Nest application instance.
+ */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const BACKEND_PORT = process.env.BACKEND_PORT ?? 3000;
+  await app.listen(BACKEND_PORT);
 }
 bootstrap();
