6888c0e63ea8f63fd78c116433b5c7866daf0289
KyNaOo <kevan122.wu@gmail.com>
2024-07-01 16:31:27 +0200

    feat: Login screen

--- frontend/app/(authentication)/login.tsx
+++ frontend/app/(authentication)/login.tsx
@@ -1,45 +1,108 @@
-import {View, Text, StyleSheet, SafeAreaView} from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
-export default function LoginScreen () {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.linkText}>Coucou</Text>
-        </SafeAreaView>
-    );
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [mdp, setMdp] = useState('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <Image
+          source={require('../../assets/images/logo-transparent.png')}
+          style={styles.image}
+        />
+        <Text style={styles.title}>Connexion</Text>
+        <Text style={styles.description}>
+          Connectez vous à Plavon et planifiez tous vos évènements !
+        </Text>
+      </View>
+      <View style={styles.middleContainer}>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+          style={styles.input}
+          theme={{ roundness: 5 }} // Make the input rounded
+        />
+        <TextInput
+          label="Mot de passe"
+          value={mdp}
+          onChangeText={(mdp) => setMdp(mdp)}
+          style={styles.input}
+          secureTextEntry
+          theme={{ roundness: 5 }} // Make the input rounded
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          mode="contained"
+          buttonColor="#EFB4E9"
+          labelStyle={styles.buttonText}
+          style={styles.button}
+          onPress={() => {
+            // Handle login logic
+          }}
+        >
+          Connexion
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
 };
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#605790',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 20,
-    },
-    image: {
-      width: 300, // Adjust the width as needed
-      height: 300, // Adjust the height as needed
-      resizeMode: 'contain', // Ensure the image scales uniformly
-      marginBottom: 20, // Add some margin below the image if needed
-    },
-    buttonContainer: {
-      width: '50%',
-      marginBottom: 20,
-    },
-    button: {
-      marginVertical: 15,
-    },
-    linkContainer: {
-      position: 'absolute',
-      bottom: 20,
-    },
-    linkText: {
-      color: '#F595F2',
-      textDecorationLine: 'underline',
-      fontFamily: 'PoppinsRegular',
-    },
-    buttonText: {
-      fontFamily: 'PoppinsRegular',
-      fontSize: 16,
-    },
-  });
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#605790',
+    paddingHorizontal: 20,
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70, // Adjust padding to move content closer to the top
+  },
+  middleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    width: '100%',
+    alignItems: 'center',
+    paddingBottom: 20, // Ensure button is at the bottom
+  },
+  button: {
+    width: '90%',
+    marginVertical: 15,
+  },
+  buttonText: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 16,
+    padding: 5,
+  },
+  title: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 45,
+    color: '#F595F2',
+  },
+  description: {
+    fontFamily: 'PoppinsRegular',
+    color: 'white',
+    textAlign: 'center',
+  },
+  input: {
+    width: '80%',
+    marginBottom: 40,
+  },
+});
+
+export default Login;
