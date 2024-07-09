715c515509799d56567b110a3bccad537e694a96
Noah Daen <noah.daen@webedia-group.com>
2024-07-09 20:35:29 +0200

    feat: add new ressources for interest entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '../db/data-source';
 import { UserModule } from './user/user.module';
+import { InterestModule } from './interest/interest.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+    InterestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
