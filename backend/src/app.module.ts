7ab29fa6ddc0f86735a91d1ba73dfb3b98d1edb3
LOPEZ Gabriel <gabriel.lopez@epitech.eu>
2024-07-09 16:59:34 +0200

    feat(:card_file_box:): Create the Message entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '../db/data-source';
 import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/message/entities/message.entity.ts
@@ -0,0 +1,30 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    JoinColumn,
+} from 'typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Group } from 'src/groups/entities/group.entity';
+
+@Entity()
+export class Message {
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column('text')
+    content: string;
+
+    @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+    timestamp: Date;
+
+    @ManyToOne(() => Group, group => group.messages)
+    @JoinColumn({ name: 'id_group' })
+    group: Group;
+
+    @ManyToOne(() => User, user => user.messages)
+    @JoinColumn({ name: 'id_user' })
+    user: User;
+
+}
