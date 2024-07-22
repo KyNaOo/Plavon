dd586bd4910190b96bb200b7eeba5bf0a8f1ec1b
LOPEZ Gabriel <gabriel.lopez@epitech.eu>
2024-07-22 21:27:10 +0200

    :bug: fix( Message ): Module definition

--- backend/src/message/message.module.ts
+++ backend/src/message/message.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageGateway } from './message.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from './entities/message.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Group } from 'src/groups/entities/group.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Group, User, Message])],
   providers: [MessageGateway, MessageService],
+  exports: [MessageService],
 })
 export class MessageModule {}
