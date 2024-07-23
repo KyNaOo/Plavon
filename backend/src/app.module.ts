6aff0e98900294cad4c3672ecfb23d130130d376
LOPEZ Gabriel <gabriel.lopez@epitech.eu>
2024-07-23 18:17:53 +0200

    :wrench: config( BullMQ ): - Add the Redis config for bullmq

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,17 +8,28 @@ import { MessageModule } from './message/message.module';
 import { GroupsModule } from './groups/groups.module';
 import { AuthModule } from './auth/auth.module';
 import { InterestModule } from './interest/interest.module';
+import { BullModule } from '@nestjs/bullmq';
+import { NotificationGateway } from './notification/notification.gateway';
+import { NotificationService } from './notification/notification.service';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST ?? 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379'),
+      },
+    }),
     UserModule,
     MessageModule,
     GroupsModule,
     AuthModule,
     InterestModule,
+    NotificationModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NotificationGateway, NotificationService],
 })
 export class AppModule {}
