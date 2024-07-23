b79a458fa566e746dc0ec3fd913b75cd1c1140cc
Gabriel Lopez <91618272+glopez-dev@users.noreply.github.com>
2024-07-23 11:20:27 +0200

    Add an OpenAPI Swagger for the backend (#57)
    * :heavy_plus_sign: dependancies: Add `@nestjs/swagger`
    
    * :memo: docs: Add an OpenAPI Swagger
    
    * :sparkles: feat( Swagger ): Add an API endpoint for the JSON
    
    * :wrench: config( nest-cli.json ):
    - Setup the OpenAPI CLI plugin
    
    * :memo: docs( Swagger ): Add a tag for each resource.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as fs from 'node:fs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 config();
 
@@ -15,6 +16,12 @@ const httpsOptions = {
   cert: fs.readFileSync('./secrets/cert.crt'),
 };
 
+const swaggerConfig = new DocumentBuilder()
+  .setTitle('Plavon API')
+  .setDescription('The Plavon API description')
+  .setVersion('1.0')
+  .build();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
@@ -22,6 +29,11 @@ async function bootstrap() {
 
   const logger = new Logger('Bootstrap');
   const BACKEND_PORT = process.env.BACKEND_PORT ?? 3000;
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
